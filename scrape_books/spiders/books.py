6739fe66957b295de913f75f4e6a2e84678aecf5
andriihrechko <andriihrechko250703@gmail.com>
2026-07-12 18:10:48 +0300

    create spider features for scraping all categories, categories page with pagination and single book

--- scrape_books/spiders/books.py
+++ scrape_books/spiders/books.py
@@ -1,5 +1,16 @@
 import scrapy
 
+from scrape_books.items import ScrapeBooksItem
+
+
+RATING_MAP = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5,
+}
+
 
 class BooksSpider(scrapy.Spider):
     name = "books"
@@ -7,4 +18,44 @@ class BooksSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        category_urls = response.css("ul.nav-list ul li a::attr(href)").getall()
+        for url in category_urls:
+            yield response.follow(url, self.parse_category)
+
+    def parse_category(self, response):
+        book_urls = response.css("article.product_pod h3 a::attr(href)").getall()
+        for book_url in book_urls:
+            yield response.follow(book_url, self.parse_book)
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse_category)
+
+    def parse_book(self, response):
+        title = response.css("div.product_main h1::text").get()
+        price = response.css("p.price_color::text").get()
+        price = float(price.replace("£", ""))
+
+        stock = response.css("p.instock.availability::text").re_first(r"\d+")
+        if stock:
+            stock = int(stock)
+
+        rating = response.css("p.star-rating::attr(class)").get()
+        rating = RATING_MAP[rating.split()[-1]]
+
+        category = response.css("ul.breadcrumb li:nth-child(3) a::text").get()
+        description = response.css("#product_description + p::text").get()
+
+        upc = response.css("table.table-striped tr:nth-child(1) td::text").get()
+
+        book = ScrapeBooksItem(
+            title=title,
+            price=price,
+            amount_in_stock=stock,
+            rating=rating,
+            category=category,
+            description=description,
+            upc=upc
+        )
+
+        yield book
