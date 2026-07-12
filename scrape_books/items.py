2afcb33c0814726a522bae372c58ac98a9776f43
andriihrechko <andriihrechko250703@gmail.com>
2026-07-12 18:28:59 +0300

    change config

--- scrape_books/items.py
+++ scrape_books/items.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class ScrapeBooksItem:
     title: str
     price: float
-    amount_in_stock: int
+    amount_in_stock: Optional[int]
     rating: int
     category: str
     description: str
